90e02269e697b05b2add8c537f2dfde6377694b8
Tim-Abwao <abwaomusungu@gmail.com>
2020-08-15 17:23:14 +0300

    Updating sample limits and adding doc strings

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import json
 with open('distributions.json') as file:
     dist_data = json.load(file)
 
-param_ticks = [0, 0.5, 1, 2.5, 5, 7.5, 10]
+param_ticks = [0, 0.5, 1, 2.5, 5, 7.5, 10]  # displayed on parameter sliders
 
 
 app = dash.Dash(__name__, title="Statistical Distributions Sampler",
@@ -27,7 +27,9 @@ app.layout = html.Div([
     set parameters, and see their effect in real time.
     """]),
     html.Div([
+        # Container for distribution description
         html.Div(id='description', className='description'),
+
         # Distribution and parameter selection
         html.Div(id='parameters', children=[
             # Distribution drop-down menu
@@ -42,23 +44,29 @@ app.layout = html.Div([
             dcc.Slider(id='parameter1', included=False, min=0.05, max=10,
                        step=0.01, value=5, tooltip={'placement': 'top'},
                        marks={i: {'label': f'{i}'} for i in param_ticks}),
+
             # Parameter 2 slider
             html.Label(id='param2name', className='param-label',
                        htmlFor='parameter2'),
             dcc.Slider(id='parameter2', included=False, min=0.05, max=10,
                        step=0.01, value=5, tooltip={'placement': 'top'},
                        marks={i: {'label': f'{i}'} for i in param_ticks}),
+
             # Sample size slider
             html.Label("Sample size (n):", className='param-label',
                        htmlFor='sample-size'),
-            dcc.Slider(id='sample-size', min=10, max=500, value=20,
+            dcc.Slider(id='sample-size', min=10, max=300, value=100,
                        step=10, included=False, tooltip={'placement': 'top'},
                        marks={i: {'label': f'{i}'}
                               for i in range(0, 500, 50)})
             ]),
+
+        # Summary-statistics table
         html.Div([html.Table(id='summary-stats'),
                   html.P(id='current-params')], className='stats'),
         ], className='parameters'),
+
+    # Container for the graphs
     html.Div([
         html.Div(dcc.Graph(id='histogram')),
         html.Div(dcc.Graph(id='violinplot')),
@@ -71,44 +79,15 @@ app.layout = html.Div([
                Output('parameter2', 'disabled')],
               [Input('select-distribution', 'value')])
 def set_parameters(distribution):
+    """
+    Gets and sets the parameter names in both parameter sliders, given a
+    particular distribution. Also, disables the second parameter slider if
+    the distribution has only one parameter.
+    """
     dist = dist_data[distribution]
-    num_params = dist['num_params']
     param1_name, param2_name = dist['param1'], dist['param2']
+    num_params = dist['num_params']
     return param1_name, param2_name, True if num_params < 2 else False
-
-
-@app.callback([Output('histogram', 'figure'),
-               Output('violinplot', 'figure'),
-               Output('summary-stats', 'children')],
-              [Input('select-distribution', 'value'),
-               Input('sample-size', 'value'),
-               Input('parameter1', 'value'),
-               Input('parameter2', 'value')])
-def create_and_plot_sample(distribution, size, *parameters):
-    sample = get_random_sample(distribution, size, parameters)
-
-    fig1 = px.histogram(x=sample, marginal='rug', opacity=0.5,
-                        color_discrete_sequence=['teal'],
-                        title=f'{distribution} Sample Histogram')
-
-    fig2 = px.violin(x=sample, box=True, color_discrete_sequence=['teal'],
-                     title=f'{distribution} Sample Violin Plot')
-
-    sample_stats = [html.Th('Summary Statistics')] + \
-                   [html.Tr([html.Td(f'{name}:'), html.Td(value)])
-                    for name, value in descriptive_stats(sample).items()]
-
-    return fig1, fig2, sample_stats
-
-
-@app.callback(Output('description', 'children'),
-              [Input('select-distribution', 'value')])
-def show_description(distribution):
-    return [html.H3(f'Current selection: {distribution} Distribution')] + \
-           [html.P(desc)
-            for desc in [dist_data[distribution]['summary'].split('>')] +
-            [html.A('Learn more...', className='wiki-link',
-             href=dist_data[distribution]['wiki_link'])]]
 
 
 @app.callback([Output('parameter1', 'max'),
@@ -116,6 +95,9 @@ def show_description(distribution):
               [Input('param1name', 'children'),
                Input('param2name', 'children')])
 def scale_probability_slider(*params):
+    """
+    Rescales a parameter slider to the range (0, 1) if it is a probability
+    """
     def check_if_probability(name):
         return 1 if 'Prob' in name else 10
     new_max = tuple(check_if_probability(x) for x in params)
@@ -129,11 +111,52 @@ def scale_probability_slider(*params):
                Input('parameter2', 'value'),
                Input('sample-size', 'value')])
 def display_current_params(nam1, val1, nam2, val2, n):
+    """Displays the current parameters: their names and values"""
     if nam2 == 'N/A':
         nam2 = val2 = ''
     return [html.B('Parameters: '),
             f'{nam1}: {val1}, {nam2}: {val2}, Sample size: {n}']
 
 
+@app.callback(Output('description', 'children'),
+              [Input('select-distribution', 'value')])
+def show_description(distribution):
+    """Parses the distribution summary information as paragraphs"""
+    return [html.H3(f'Current selection: {distribution} Distribution')] + \
+           [html.P(desc)
+            for desc in [dist_data[distribution]['summary'].split('>')] +
+            [html.A('Learn more...', className='wiki-link',
+             href=dist_data[distribution]['wiki_link'])]]
+
+
+@app.callback([Output('histogram', 'figure'),
+               Output('violinplot', 'figure'),
+               Output('summary-stats', 'children')],
+              [Input('select-distribution', 'value'),
+               Input('sample-size', 'value'),
+               Input('parameter1', 'value'),
+               Input('parameter2', 'value')])
+def create_and_plot_sample(distribution, size, *parameters):
+    """
+    Creates a random sample of the selected distribution using the specified
+    parameters, then plots a histogram and a violin plot. Finally, descriptive
+    statistics are computed and packaged as a table.
+    """
+    sample = get_random_sample(distribution, size, parameters)
+
+    fig1 = px.histogram(x=sample, marginal='box', opacity=0.5,
+                        color_discrete_sequence=['teal'],
+                        title=f'{distribution} Sample Histogram')
+
+    fig2 = px.violin(x=sample, box=True, color_discrete_sequence=['teal'],
+                     title=f'{distribution} Sample Violin Plot')
+
+    sample_stats = [html.Th('Summary Statistics')] + \
+                   [html.Tr([html.Td(f'{name}:'), html.Td(value)])
+                    for name, value in descriptive_stats(sample).items()]
+
+    return fig1, fig2, sample_stats
+
+
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True)
