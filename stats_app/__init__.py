65accca0c3b76307944ccf1c92cec4daee78ed3d
Tim-Abwao <abwaomusungu@gmail.com>
2021-05-07 02:32:39 +0300

    Set logging level to ERROR

--- stats_app/__init__.py
+++ stats_app/__init__.py
@@ -1,3 +1,10 @@
 from stats_app.dash_app import app
+import logging
 
+
+# Set waitress server logging level to ERROR
+logger = logging.getLogger('waitress')
+logger.setLevel(logging.ERROR)
+
+print('Running at http://0.0.0.0:8080 (default)')
 server = app.server
