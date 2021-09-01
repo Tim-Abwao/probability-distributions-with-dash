f8087064ca242fd79ef1abb4e07a8c36e2171365
Tim-Abwao <abwaomusungu@gmail.com>
2021-09-01 19:56:22 +0300

    Update utility functions
    Add type annotations.
    Update docstrings.
    Reduce cruft.

--- distributions_dashboard/utils.py
+++ distributions_dashboard/utils.py
@@ -21,7 +21,7 @@ distribution_functions = {
 }
 
 
-def process_parameters(distribution, parameters):
+def process_parameters(distribution: str, parameters: list) -> list:
     """Validate the parameters to ensure they are appropriate for the given
     distribution.
 
@@ -34,14 +34,14 @@ def process_parameters(distribution, parameters):
 
     Returns
     -------
-    A list of valid parameter values.
+    list
+        Parameter values.
     """
-    # The default value for parameter2 in distributions having no second
+    # The default value for `parameter2` in distributions having a single
     # parameter is `None`.
     param_list = [param for param in parameters if param is not None]
 
     if distribution in {"Bernoulli", "Geometric"}:
-        # These have only one parameter - probability
         # Probability must be in the range [0, 1]
         return param_list if 0 <= param_list[0] <= 1 else [0.5]
     elif distribution in {"Binomial", "Negative Binomial"}:
@@ -54,17 +54,18 @@ def process_parameters(distribution, parameters):
         return param_list
 
 
-def get_summary_statistics(data):
+def get_summary_statistics(data: pd.Series) -> dict:
     """Get basic descriptive statistics.
 
     Parameters
     ----------
-    data : array-like
+    data : pandas.core.series.Series
         An array of numerical values.
 
     Returns
     -------
-    A dictionary of various summary statistics.
+    dict
+        Summary statistics.
     """
     q1, q2, q3 = np.quantile(data, [0.25, 0.5, 0.75])
     summary_stats = {
@@ -81,7 +82,9 @@ def get_summary_statistics(data):
     return {key: round(value, 4) for key, value in summary_stats.items()}
 
 
-def process_random_sample(distribution, size, parameters):
+def process_random_sample(
+    distribution: str, size: int, parameters: list
+) -> dict:
     """Generate a sample of the specified probability distribution using the
     given parameters, then compute summary statistics.
 
@@ -96,18 +99,17 @@ def process_random_sample(distribution, size, parameters):
 
     Returns
     -------
-    A random sample of the specified probability distribution as a numpy array,
-    plus the parameters applied, and summary statistics.
+    dict
+        A random sample of the specified probability distribution as a numpy
+        array, plus the parameters applied, and summary statistics.
     """
-    # Validate parameters
     parameters = process_parameters(distribution, parameters)
-    # Get a random sample
-    sample = pd.Series(
+    sample_data = pd.Series(
         distribution_functions[distribution].rvs(*parameters, size=size),
         name=f"{distribution}-sample",
     )
     return {
-        "data": sample,
+        "data": sample_data,
         "parameters": parameters,
-        "summary_statistics": get_summary_statistics(sample),
+        "summary_statistics": get_summary_statistics(sample_data),
     }
